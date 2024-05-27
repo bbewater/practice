3bd438cfcfc3366845a541b9a3577b8ef8aeb3db
zhoudun <zhoudun@unitechs.com>
2024-05-27 10:33:11 +0800

    interrupt结合LockSupport

--- daily/src/main/java/com/sz/bewater/practice/interview/MyInterruptLock.java
+++ daily/src/main/java/com/sz/bewater/practice/interview/MyInterruptLock.java
@@ -1,10 +1,12 @@
 package com.sz.bewater.practice.interview;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MyInterruptLock {
     private static final ReentrantLock lock = new ReentrantLock();
+    private static Thread thread2;
 
     //lock相较于synchronized，lock可以中断获取锁(等待获取锁的时候被中断)，而synchronized不行
 //    lock.interrupt();  效果相当于lock.tryLock(timeout)  一定时间获取不到 我就不获取了  避免线程长时间阻塞 可以去处理别的任务
@@ -18,6 +20,7 @@ public class MyInterruptLock {
                 TimeUnit.SECONDS.sleep(2);  //模拟线程工作
                 lock.unlock();
                 System.out.println(Thread.currentThread().getName() + "释放了锁");
+                LockSupport.unpark(thread2);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -25,7 +28,7 @@ public class MyInterruptLock {
 
         }, "thread1");
 
-        Thread thread2 = new Thread(() -> {
+        thread2 = new Thread(() -> {
             try {
                 TimeUnit.MILLISECONDS.sleep(100);   //确保线程1先获取锁
                 lock.lockInterruptibly();   //表示可被中断的获取锁
@@ -35,6 +38,17 @@ public class MyInterruptLock {
                 System.out.println(Thread.currentThread().getName() + "释放了锁");
             } catch (InterruptedException e) {
                 System.out.println(Thread.currentThread().getName() + "线程被中断");
+                try {
+                    LockSupport.park();
+                    lock.lockInterruptibly();   //表示可被中断的获取锁
+                    System.out.println(Thread.currentThread().getName() + "重新获取了锁");
+                    TimeUnit.SECONDS.sleep(2);  //模拟线程工作
+                    lock.unlock();
+                    System.out.println(Thread.currentThread().getName() + "释放了锁");
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+
 //                被中断后 可以去干别的任务
 //                doOtherWork();
 //                throw new RuntimeException(e);
