7a0b39ff76e1d26f45b04762f9428eb642bbdcac
zhoudun <zhoudun@unitechs.com>
2024-08-05 21:52:46 +0800

    补充

--- daily/src/main/java/com/sz/bewater/practice/interview/juc/CompletableFuture/MyTest.java
+++ daily/src/main/java/com/sz/bewater/practice/interview/juc/CompletableFuture/MyTest.java
@@ -4,10 +4,16 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class MyTest {
-//  whenComplete和  handle 都会返回一个新的CompletableFuture  但是whenComplete返回的新的CompletableFuture和传进来的future返回值必须相同
+//    runAsync 用于异步执行一个不返回结果的任务。
+//    supplyAsync 用于异步执行一个返回结果的任务。
+
+
+//  whenComplete和  handle 都会返回一个新的CompletableFuture
+//  但是whenComplete返回的新的CompletableFuture和传进来的future返回值必须相同
 //    而handle可以返回与传进来的future返回值不同的CompletableFuture
 
-//    thenApply、thenAccept 都可以原future进行消费 但是thenApply可以返回一个新的带返回值的CompletableFuture而thenAccept不可以
+//    thenApply、thenAccept 都可以原future进行消费
+//    但是thenApply可以返回一个新的带返回值的CompletableFuture而thenAccept不可以
 //    thenRun 不依赖于原future的返回值 强调的是原future完成后要做的事
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
