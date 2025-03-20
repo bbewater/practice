9810392932628feb71e1ad1feadb1d9d333a1fab
zhoudun <17768025565@163.com>
2025-03-20 17:24:20 +0800

    ConcurrentHashMap 的补充

--- daily/src/main/java/com/sz/bewater/practice/interview/container/MyConcurrentHashMap.java
+++ daily/src/main/java/com/sz/bewater/practice/interview/container/MyConcurrentHashMap.java
@@ -1,5 +1,6 @@
 package com.sz.bewater.practice.interview.container;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class MyConcurrentHashMap {
@@ -22,5 +23,14 @@ public class MyConcurrentHashMap {
 //        }
         // 1.保证读效率(写的时候 读不能阻塞(写数据的时候 树的结构可能会发生变化 这时候不能从树上读 但是也不能阻塞住等写操作完成 可以退而求其次去双向链表上读) 去双向链表上去读)
         // 2.扩容(红黑树的扩容 依赖这个双向链表 使扩容成本降低)
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+        map.put("1", "1");
+        map.put("2", "2");
+        System.out.println(map.size());
+        System.out.println(map.mappingCount());
+        //merge 对于原始 k,v 进行操作 假如 k 不存在也不会报错 而是会以结果 put 进去一个新的 kv
+        map.merge("1", "3", (oldValue, newValue) -> oldValue + newValue); //1:13
+        map.merge("3", "3", (oldValue, newValue) -> oldValue + newValue);   //3:3
+        map.forEach((k,v) -> System.out.println(k + ":" + v));
     }
 }
