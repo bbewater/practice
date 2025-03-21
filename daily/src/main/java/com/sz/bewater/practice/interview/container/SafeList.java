c342c736e67ed1b503ccf415d066a4159d038dfb
zhoudun <17768025565@163.com>
2025-03-21 16:28:36 +0800

    CopyOnWriteList 迭代器弱一致性

--- daily/src/main/java/com/sz/bewater/practice/interview/container/SafeList.java
+++ daily/src/main/java/com/sz/bewater/practice/interview/container/SafeList.java
@@ -1,6 +1,7 @@
 package com.sz.bewater.practice.interview.container;
 
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class SafeList {
 
@@ -11,5 +12,15 @@ public class SafeList {
     public static void main(String[] args) {
         List<ArrayList<Object>> list1 = Collections.singletonList(new ArrayList<>());
         List<Object> list2 = Collections.synchronizedList(new LinkedList<>());
+
+        CopyOnWriteArrayList<Integer> list3 = new CopyOnWriteArrayList<>();
+        list3.add(1);
+        list3.add(2);
+        list3.add(3);
+        Iterator<Integer> iterator = list3.iterator(); //生成 list3快照 后续或迭代过程中对 list3的修改不影响
+        list3.add(4);
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());    //不包含 4 CopyOnWriteList 迭代器弱一致性
+        }
     }
 }
